283f9e51d4afc7db7503de6744c1550d9a0eb131
Ismoil Ro'ziboyev <ismoil@mf.uz>
2024-03-10 12:54:03 +0500

    fixing

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/ismoilroziboyev/log-minder-client
 
 go 1.21.5
 
-require (
-	github.com/go-resty/resty/v2 v2.11.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
-)
+require github.com/go-resty/resty/v2 v2.11.0
+
+require golang.org/x/net v0.22.0 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 )
 
 type LogMinderClient interface {
 	WriteLog(ctx context.Context, payload *WriteLogPayload) error
 	RetreiveLogs(ctx context.Context, payload *RetreiveLogsFilter) (*RetreiveLogsResponse, error)
+}
+
+func main() {
+	fmt.Println("hello world!")
 }
